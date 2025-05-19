a42b9a15a350a7aba3f0bf691b4fd3805d3928f2
higherstatedesigns <macey.chap@outlook.com>
2025-05-19 14:48:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+document.querySelectorAll('button').forEach(btn => {
+    btn.addEventListener('click', () => {
+        alert('This is a placeholder. Cart functionality coming soon!');
+    });
+});
